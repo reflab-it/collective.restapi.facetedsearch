02fe00356dbb7dae0e4e826fe61bc493079d3e8d
muellers-saw-leipzig <muellers@saw-leipzig.de>
2021-11-25 14:43:52 +0100

    change name of modifier (conflicts with collective.taxonomy)

--- src/collective/restapi/facetedsearch/api/services/faceted_search/groupByModifiers.py
+++ src/collective/restapi/facetedsearch/api/services/faceted_search/groupByModifiers.py
@@ -8,31 +8,8 @@ from zope.schema.interfaces import IVocabularyFactory
 from collective.taxonomy.interfaces import ITaxonomy
 from plone.api import portal
 
-
+# see also collective.collectionfilter 'Overloading GroupByCriteria' how to set display_modifier function for indexes or assign a metadata column name different to the index name
 @implementer(IGroupByModifier)
 @adapter(IGroupByCriteria)
 def groupby_modifier(groupby):
     pass
-    # groupby._groupby['tax_ou_type']['display_modifier'] = lambda x: getVocabTitle(x, 'tax_ou_type')
-    # groupby._groupby['license_short_title']['display_modifier'] = lambda x: getVocabTitle(x, 'license_short_title')
-    # groupby._groupby['tax_research_fields']['display_modifier'] = lambda x: getVocabTitle(x, 'tax_research_fields')
-    # groupby._groupby['taxonomy_ddc']['display_modifier'] = lambda x: getVocabTitle(x, 'taxonomy_ddc')
-    # groupby._groupby['taxonomy_stock_categories']['display_modifier'] = lambda x: getVocabTitle(x, 'taxonomy_stock_categories')
-
-
-def getVocabTitle(token, metadata_name):
-    mappings = portal.get_registry_record(name='saw.facetedsearch.metadata_vocab_mapping', default={})
-    if metadata_name in mappings:
-        vocabname = mappings[metadata_name]
-        taxonomy = queryUtility(ITaxonomy, name=vocabname)
-        if taxonomy:
-            title = taxonomy.translate(token)
-            return title
-        else:
-            vocab = queryUtility(IVocabularyFactory, name=vocabname)
-            if vocab:
-                term = vocab().getTermByToken(token)
-                if term and term.title:
-                    return term.title
-
-    return token
