3a8b941bd39dabab3f93cf5e6b93a51a60e3c358
Michelangelo Giacomelli <michelangelog@gmail.com>
2023-10-16 11:40:05 +0200

    Update groupByModifiers.py

--- src/collective/restapi/facetedsearch/api/services/faceted_search/groupByModifiers.py
+++ src/collective/restapi/facetedsearch/api/services/faceted_search/groupByModifiers.py
@@ -5,7 +5,7 @@ from zope.component import adapter
 from zope.interface import implementer
 from zope.component import queryUtility
 from zope.schema.interfaces import IVocabularyFactory
-from collective.taxonomy.interfaces import ITaxonomy
+#from collective.taxonomy.interfaces import ITaxonomy
 from plone.api import portal
 
 # see also collective.collectionfilter 'Overloading GroupByCriteria' how to set display_modifier function for indexes or assign a metadata column name different to the index name
